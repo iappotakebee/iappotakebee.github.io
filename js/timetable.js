7e7f3401d2167b41ff080b0b82650f96df75e6ad
iappotakebee <sakuya3.1341.popnmusic@gmail.com>
2022-08-09 10:17:19 -0400

    [fix] fixed unexpectedly replaced files

--- js/timetable.js
+++ js/timetable.js
@@ -1,55 +1,121 @@
-function construstTimeTable(timeTable, talksData) {
+function construstTable(timeTable, achievementData, flg_showamount) {
+  var cnt = 0;
   return Object.keys(timeTable)
-               .filter(function (k) {return timeTable[k];})
-               .sort()
-               .map(function (time) {
-                 // console.log(time);
-                 var name = timeTable[time];
-                 // console.log(name);
-                 var index = 0;
-
-                 // if (name == "なおと") {
-                 //   name = "宇佐美尚人";
-                 // } else if (name.indexOf("こすも") == 0) {
-                 //   index = parseInt(name.charAt(3)) - 1;
-                 //   name = "宇佐美こすも";
-                 // }
-                 var talk = talksData.filter(function (t) { return t.name.indexOf(name) == 0; })[index];
-
-                 if ((time == "09:50") || (time == "12:50") || (time == "14:20")) 
-                 {
-                    return { time: name, name: "", title: "", major: ""};
-                  } 
-                  else if ((time == "12:20") || (time == "14:00") || (time == "15:30")|| (time == "17:40")) {
-                    return { time: "\xa0", name: "  ", title: "", major: ""};
-                  }
-                  else if (name == "藏田2") {
-                    return { time: time, name: "藏田 玲美", title: "「薬」ができるまで～創薬開発から承認まで～", major: "骨免疫学、薬物治療学"};
-                  }
-                  else if (time == "16:30"){
-                    return { time: time, name: "", title: name, major: ""}
-                  }
-                  else if (time == "17:30"){
-                    return { time: "", name: "", title: name, major: ""}
-                  }
-                 else 
-                  {
-                    return { time: time, name: talk.name, title: talk.title, major: talk.affiliation };
-                  }
-               });
+    .filter(function (k) {return timeTable[k];})
+    .reverse()
+    .map(function (time, id) {
+      var name = timeTable[time];
+      var index = 0;
+      var tmp_comment = "";
+      var tmp_type = "";
+      var tmp_availability = "";
+      var item = achievementData.filter(function (t) { 
+        return t.name.indexOf(name) == 0; 
+      })[index];
+      if (
+        time.indexOf("9999") > 1
+      ) 
+      {
+        return {
+          idx: "", affiliation: "", month: name, title: "", 
+          endperiod: "", year: "",
+          comment:"", type: "", url: ""
+        };
+      } 
+      else if (
+        time.indexOf("0000") > 1
+      ) 
+      {
+        return {
+          idx: "", affiliation: "", month: "\xa0", title: "", 
+          endperiod: "", year: "",
+          comment:"", type: "", url: ""
+        };
+      } 
+      else {
+        cnt++;
+        // create temporary comment
+        if (flg_showamount != false)
+        {
+          if (item.amount !== ""){
+            tmp_comment = item.amount;
+          }
+        }
+        if (item.period !== ""){
+          if (tmp_comment !== ""){
+            tmp_comment = tmp_comment+", "+item.period;
+          }
+          else{
+            tmp_comment = item.period;
+          }
+        }
+        if (item.comment !== ""){
+          if (tmp_comment !== ""){
+            tmp_comment = tmp_comment+", "+item.comment;
+          }
+          else{
+            tmp_comment = item.comment;
+          }
+        }
+        if (tmp_comment !== ""){
+          tmp_comment = "("+tmp_comment+")";
+        }
+        // create temporary type
+        if (item.type != "")
+        {
+          tmp_type = "["+item.type+"]";
+        }
+        // create temporary url
+        if (item.url != "")
+        {
+          return { 
+            idx: cnt+".", affiliation: item.affiliation, month: item.month, 
+            title_link: item.title,
+            endperiod: item.endperiod, year: item.year, 
+            comment: tmp_comment, type: tmp_type,  url: item.url ,
+            availability: tmp_availability
+          };
+        }
+        else
+        {
+          return { 
+            idx: cnt+".", affiliation: item.affiliation, month: item.month, 
+            title: item.title,
+            endperiod: item.endperiod, year: item.year, 
+            comment: tmp_comment, type: tmp_type,  url: item.url ,
+            availability: tmp_availability
+          };
+        }
+      }
+    });
 }
 
 
 $(function () {
-  var firstDayTable = construstTimeTable(day1, data);
-  var secondDayTable = construstTimeTable(day2, data);
-  // var thirdDayTable = construstTimeTable(day3, data);
-
+  var table_awards = construstTable(time_awards, data_awards);
+  var table_resgrnt= construstTable(time_resgrnt, data_resgrnt);
+  var table_synchroradleader= 
+    construstTable(time_synchroradleader, data_synchroradleader);
+  var table_synchroradmember= 
+    construstTable(time_synchroradmember, data_synchroradmember);
+  var table_resflw = construstTable(time_resflw, data_resflw);
+  var table_scholarships = construstTable(time_scholarships, data_scholarships, false);
   var template = $('#template').html();
   Mustache.parse(template);
-  var renderedFirst = Mustache.render(template, {table: firstDayTable, header: "9/20 (日)"});
-  var renderedSecond = Mustache.render(template, {table: secondDayTable, header: "9/21 (月)"});
-  // var renderedThird = Mustache.render(template, {table: thirdDayTable, header: "11/25(日)"});
-  // $('.article-headline').html(renderedFirst + "<br />" + renderedSecond + "<br />" + renderedThird);
-  $('.article-headline').html(renderedFirst + "<br />" + renderedSecond);
+
+  var rendered_awards = Mustache.render(template, {table: table_awards, header: "Awards and honors", tag:"Award"});
+  var rendered_resgrnt = Mustache.render(template, {table: table_resgrnt, header: "Research grants", tag:"ResGrn"});
+  var rendered_synchroradleader = Mustache.render(template, {table: table_synchroradleader, header: "SR experiment proposals as a project leader", tag:"SRExpL"});
+  var rendered_synchroradmember = Mustache.render(template, {table: table_synchroradmember, header: "SR experiment proposals as a project member", tag:"SRExpM"});
+  var rendered_resflw = Mustache.render(template, {table: table_resflw, header: "Research fellowships", tag:"ResFll"});
+  var rendered_scholarships = Mustache.render(template, {table: table_scholarships, header: "Scholarships etc.", tag:"Schlrs"});
+  $('.article-headline').html(
+    rendered_awards + "<br />" + 
+    rendered_resgrnt + "<br />" + 
+    rendered_synchroradleader + "<br />" + 
+    rendered_synchroradmember + "<br />" + 
+    rendered_resflw + "<br />" + 
+    rendered_scholarships 
+  );
 });
+
